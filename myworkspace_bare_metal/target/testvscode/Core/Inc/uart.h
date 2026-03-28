48de3b2234ab6c2b1275f1223a388529f0064839
jesus doza <jesus.doza@gmail.com>
2026-03-28 16:48:43 -0500

    refactor uart.h

--- myworkspace_bare_metal/target/testvscode/Core/Inc/uart.h
+++ myworkspace_bare_metal/target/testvscode/Core/Inc/uart.h
@@ -1,10 +1,9 @@
-// prototypes
 #ifndef __UART_H
 #define __UART_H
+#endif // __UART_H
 
 #include "stm32f446xx.h"
 #include <stdint.h>
 
+// prototypes
 void usart2_tx_init(void);
-
-#endif // __UART_H
