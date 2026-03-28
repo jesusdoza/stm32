7bf392c949811ff327b4a6693f5519e2302bd8fe
jesus doza <jesus.doza@gmail.com>
2026-03-28 13:48:32 -0500

    delete old

--- myworkspace_bare_metal/target/testvscode/Core/Src/main.c
+++ myworkspace_bare_metal/target/testvscode/Core/Src/main.c
@@ -3,32 +3,35 @@
 #include <stdint.h>
 #include <stdio.h>
 
+// ports enable
 #define GPIOAEN (1U << 0)
 #define USART2EN (1U << 17)
 
-#define CR1_TE (1U << 3) // usart CR1 transmitter enable
-#define CR1_UE = (1u << 13)
+// usart control registers
+#define CR1_TE (1U << 3)  // usart CR1 transmitter enable
+#define CR1_UE (1U << 13) // usart enable
+#define SR_TXE (1U << 7)  // USART SR Transmiter empty status register
 
-#define SYS_FREQ 160000000
+#define SYS_FREQ 16000000
 #define APB1_CLK SYS_FREQ
 
 #define UART_BAUDRATE 115200
 
+// prototypes
 static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk,
                               uint32_t BaudRate);
 static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate);
+void usart2_tx_init(void);
+void uart2_write(int ch);
 
+// entry point
 int main(void) {
 
-  /*
-  usart2 pins
-  PA2 TX
-  PA3 RX
-  AF7 alternat function
-
-  */
+  usart2_tx_init();
 
   while (1) {
+    uart2_write('H');
+    uart2_write('E');
   }
 }
 
@@ -36,44 +39,52 @@ void usart2_tx_init(void) {
   /*enable clock to GPIOA*/
   RCC->AHB1ENR |= GPIOAEN;
   /*config usart pins*/
-  // set PA2 to alternate function mode
-  GPIOA->MODER &= ~(0b11 << 4); // clear bits first space
-  GPIOA->MODER |= (0b10 << 4);  // then activate bits space
-
-  // set PA3
-  GPIOA->MODER &= ~(0b11 << 6); // clear bits first space
-  GPIOA->MODER |= (0b10 << 6);  // then activate bits space
+  // set PA2 to alternate function mode (bits 5:4 = 10)
+  GPIOA->MODER &= ~(3U << 4); // clear both bits 5:4
+  GPIOA->MODER |= (2U << 4);  // set to 10 (alternate function)
 
   // set PA2 alternate function to UART_tx AF07
-  GPIOA->MODER &= ~(0b1111 << 8); // clear bits first space
-  GPIOA->AFR[0] |= (0b0111 << 8);
-
-  // set PA3 alternate function to UART_rx AF7
-  GPIOA->MODER &= ~(0b1111 << 12); // clear bits first space
-  GPIOA->AFR[0] |= (0b0111 << 12);
-
-  // config uart
+  GPIOA->AFR[0] |= (1U << 8);
+  GPIOA->AFR[0] |= (1U << 9);
+  GPIOA->AFR[0] |= (1U << 10);
+  GPIOA->AFR[0] &= ~(1U << 11);
 
   // configure uart module clock access
   RCC->APB1ENR |= USART2EN;
 
   // config uart baudrate
 
-  uart_set_baudrate(USART2, APB1_CLK, UART_BAUDRATE)
+  uart_set_baudrate(USART2, APB1_CLK, UART_BAUDRATE);
 
-      // config transfer direction
-      USART2->CR1 = CR1_TE;
-  USART2->CR1 = CR1_UE;
+  // config transfer directio
+  USART2->CR1 = CR1_TE;
+  USART2->CR1 |= CR1_UE;
 
   // enable uart module
+  return;
 }
 
 static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk,
                               uint32_t BaudRate) {
   USARTx->BRR = compute_uart_bd(PeriphClk, BaudRate);
+
+  return;
 }
 
 static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate) {
 
   return ((PeriphClk + (BaudRate / 2U)) / BaudRate);
+}
+
+void uart2_write(int ch) {
+  // make user transmit data register is empty before writing to it
+  // wait until transmit data register is empty
+  // check the status of the transmit data register empty flag in the USART_SR
+  // register before writing to the USART_DR register
+  // wait until transmit data register is empty
+  while (!(USART2->SR & SR_TXE)) {
+    // wait
+  }
+  USART2->DR = (ch & 0xFF); // write character in the 8 bit data register
+  return;
 }
