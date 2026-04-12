f082ff44747153898245531cb367cff354989323
jesus doza <jesus.doza@gmail.com>
2026-04-12 17:37:55 -0500

    add systick driver delay toggle led

---
+++ myworkspace_bare_metal/target/baremetal/Core/Inc/systick.h
@@ -0,0 +1,5 @@
+#ifndef SYSTICK_H
+#define SYSTICK_H
+
+void systickDelayMs(int n);
+#endif /* SYSTICK_H */
--- myworkspace_bare_metal/target/baremetal/Core/Src/main.c
+++ myworkspace_bare_metal/target/baremetal/Core/Src/main.c
@@ -1,23 +1,29 @@
 #include "main.h"
 #include "adc.h"
+#include "systick.h"
 #include "uart.h"
 #include <stdint.h>
 #include <stdio.h>
 
-uint32_t sensor_value = 0;
+#define GPIOAEN (1u << 0) // enable clock for GPIOA
+#define GPIOA_5 (1u << 5) // pin 5 of GPIOA
+#define LED_PIN GPIOA_5
+
 // entry point
 int main(void) {
 
-  // usart2_tx_init();
+  // give clock access to GPIOA
+  RCC->AHB1ENR |= GPIOAEN;
+  // set pin 5 of GPIOA as output
+  GPIOA->MODER &= ~(0b11 << 10); // clear pin 5 mode to 00 (input) / reset state
+  GPIOA->MODER |= (0b01 << 10);  // set pin 5 mode to 01 (output)
+
   usart2_rxtx_init();
 
-  // Initialize ADC on PA1
-  pa1_init();
-  startConversion();
-
   while (1) {
-    sensor_value = adc_read();
-    printf("sensor value: %d \n\r", (int)sensor_value);
+    printf("a second has passed\n\r");
+    GPIOA->ODR ^= LED_PIN; // toggle pin 5 (turn on/off LED)
+    systickDelayMs(1000);
   }
 
   return 0;
