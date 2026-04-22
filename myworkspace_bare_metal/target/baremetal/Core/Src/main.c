06a1c814cb228ae73b1c7f9b1e5115570661d279
jesus doza <jesus.doza@gmail.com>
2026-04-22 15:59:23 -0500

    using output compare timer toggle led

--- myworkspace_bare_metal/target/baremetal/Core/Src/main.c
+++ myworkspace_bare_metal/target/baremetal/Core/Src/main.c
@@ -6,34 +6,11 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define GPIOAEN (1u << 0) // enable clock for GPIOA
-#define GPIOA_5 (1u << 5) // pin 5 of GPIOA
-#define LED_PIN GPIOA_5
-
 // entry point
 int main(void) {
-
-  // give clock access to GPIOA
-  RCC->AHB1ENR |= GPIOAEN;
-  // set pin 5 of GPIOA as output
-  GPIOA->MODER &= ~(0b11 << 10); // clear pin 5 mode to 00 (input) / reset state
-  GPIOA->MODER |= (0b01 << 10);  // set pin 5 mode to 01 (output)
-
-  usart2_rxtx_init();
-  tim2_1hz_init();
+  tim2_pa5_output_compare();
 
   while (1) {
-    // while value is 0 keep waiting, once it becomes 1 we know a second has
-    // passed
-    while (!(TIM2->SR & SR_UIF)) {
-      // wait for update interrupt flag to be set (timer overflow)
-    }
-
-    // clear uif flag
-    TIM2->SR &= ~SR_UIF;
-
-    printf("a second has passed using timer not systick \n\r");
-    GPIOA->ODR ^= LED_PIN; // toggle pin 5 (turn on/off LED)
   }
 
   return 0;
