ca9908cf8537dcb86e1d4d0f828d6333320a4f21
jesus doza <jesus.doza@gmail.com>
2026-04-24 14:05:39 -0500

    Squashed commit of the following:
    commit 07f7b72a69814dbc126982f81cc1c8d6daa0838f
    Author: jesus doza <jesus.doza@gmail.com>
    Date:   Fri Apr 24 14:04:20 2026 -0500
    
        input capture driver

--- myworkspace_bare_metal/target/baremetal/Core/Inc/tim.h
+++ myworkspace_bare_metal/target/baremetal/Core/Inc/tim.h
@@ -3,7 +3,8 @@
 
 void tim2_1hz_init(void);
 void tim2_pa5_output_compare(void);
+void tim3_pa6_input_capture(void);
 
-#define SR_UIF (1u << 0) // Update interrupt flag
-
-#endif /* TIM_H */
+#define SR_UIF (1u << 0)   // Update interrupt flag
+#define SR_CC1IF (1u << 1) // Capture/Compare 1 interrupt flag
+#endif                     /* TIM_H */
--- myworkspace_bare_metal/target/baremetal/Core/Src/main.c
+++ myworkspace_bare_metal/target/baremetal/Core/Src/main.c
@@ -5,12 +5,25 @@
 #include "uart.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <sys/_intsup.h>
 
+uint32_t timestamp = 0;
 // entry point
 int main(void) {
+  usart2_tx_init();
   tim2_pa5_output_compare();
-
+  tim3_pa6_input_capture();
   while (1) {
+    while (!(TIM3->SR & SR_CC1IF)) {
+      // wait for capture of edge
+    }
+
+    // read cpature value from CCR1
+    timestamp = TIM3->CCR1;
+    // clear capture flag by writing 0 to it
+    TIM3->SR &= ~SR_CC1IF;
+    // print uart message
+    printf("Captured timestamp: %lu \n\r", timestamp);
   }
 
   return 0;
